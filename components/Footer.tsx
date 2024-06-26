7befc081d5ad3b815f8791f310b66d1565c6adeb
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-06-26 14:08:47 +0200

    added footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,242 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhoneAlt,
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+import CustomButton from "./CustomButton";
+import { motion } from "framer-motion";
+
+const footerContainerVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.6,
+      duration: 0.5,
+      ease: "linear",
+    },
+  },
+};
+
+const footerItem = {
+  hidden: {
+    y: 20,
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.25, 0.6, 0.3, 0.8],
+    },
+  },
+};
 const Footer = () => {
-  return <footer className="h-[80vh] bg-orange-300">Footer</footer>;
+  return (
+    <footer className="bg-primary-300 pt-24">
+      <div className="container mx-auto pb-24">
+        <motion.div
+          variants={footerContainerVariant}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true }}
+          className="text-white grid grid-cols-1 xl:grid-cols-4 gap-x-8 gap-y-12"
+        >
+          <motion.div variants={footerItem} className="flex flex-col gap-4">
+            <Link href="#">
+              <Image
+                src={"/assets/img/logo.png"}
+                width={117}
+                height={55}
+                alt=""
+              />
+            </Link>
+            <p className="max-w-sm">
+              Empowering your fitness journey with excellence and dedication.
+            </p>
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <FaMapMarkerAlt className="text-xl text-accent" />
+                <span>Sokólska 777, Białystok</span>
+              </li>
+              <li className="flex items-center gap-4">
+                <FaPhoneAlt className="text-xl text-accent" />
+                <span>(+48) 999-999-999</span>
+              </li>
+              <li>
+                <Link className="flex items-center gap-4" href="#">
+                  <FaEnvelope className="text-xl text-accent" />
+                  <span>someemail@email.com</span>
+                </Link>
+              </li>
+            </ul>
+          </motion.div>
+          <motion.div variants={footerItem}>
+            <h4 className="h4 text-accent mb-4">Recent blog posts</h4>
+            <div className="border-b border-dotted border-gray-400 flex flex-col gap-3 pb-3 mb-4">
+              <Link className="hover:text-accent transition-all" href="#">
+                <h5 className="h5 leading-snug">
+                  Top 10 Must-Have Items in Your Gym Bag
+                </h5>
+                <p className="text-gray-400 text-[12px] tracking-[3px] uppercase">
+                  June 12, 2024
+                </p>
+              </Link>
+            </div>
+            <div className="border-b border-dotted border-gray-400 flex flex-col gap-3 pb-3 mb-4">
+              <Link className="hover:text-accent transition-all" href="#">
+                <h5 className="h5 leading-snug">
+                  How to stay motivated for all exercises
+                </h5>
+                <p className="text-gray-400 text-[12px] tracking-[3px] uppercase">
+                  May 8, 2024
+                </p>
+              </Link>
+            </div>
+            <div className="flex flex-col gap-3 pb-3 mb-4">
+              <Link className="hover:text-accent transition-all" href="#">
+                <h5 className="h5 leading-snug">
+                  A Guide to Choosing the Right Gym for You
+                </h5>
+                <p className="text-gray-400 text-[12px] tracking-[3px] uppercase">
+                  May 15, 2024
+                </p>
+              </Link>
+            </div>
+          </motion.div>
+          <motion.div variants={footerItem}>
+            <h4 className="h4 text-accent mb-4">Gallery</h4>
+            <div className="flex flex-wrap gap-2">
+              <Link href="#">
+                <Image
+                  src={"/assets/img/trainers/david.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/trainers/sofia.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/trainers/rosy.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/trainers/matt.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/footer/footer1.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/footer/footer2.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/footer/footer3.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+              <Link href="#">
+                <Image
+                  src={"/assets/img/footer/footer4.jpg"}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </Link>
+            </div>
+          </motion.div>
+          <motion.div variants={footerItem}>
+            <h4 className="h4 text-accent mb-4">Newsletter</h4>
+            <div className="flex flex-col gap-4">
+              <p className="">
+                Stay updated with our latest fitness tips, classes, and
+                exclusive offers by subscribing to our newsletter!
+              </p>
+              <form className="flex items-center">
+                <input
+                  type="text"
+                  placeholder="Your email address"
+                  className="h-[50px] outline-none px-4 text-primary-300"
+                />
+                <CustomButton containerStyles="h-[50px] px-8" text="Send" />
+              </form>
+            </div>
+          </motion.div>
+        </motion.div>
+      </div>
+      <div className="text-white border-t border-white/20 py-12">
+        <div className="container mx-auto h-full">
+          <div className="flex items-center justify-between h-full">
+            <span>&copy; Copyright 2024 FitPhysique</span>
+            <ul className="flex gap-4 text-xl">
+              <li>
+                <Link
+                  href="#"
+                  className="text-white hover:text-accent transition-al"
+                >
+                  <FaFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-white hover:text-accent transition-al"
+                >
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-white hover:text-accent transition-al"
+                >
+                  <FaYoutube />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
